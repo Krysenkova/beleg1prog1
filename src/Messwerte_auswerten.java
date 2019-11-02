aa7b198333863790947188868b0ae5fe14d8a47f
kate <you@example.com>
2019-11-02 11:25:23 +0100

    added max and min of an array

--- src/Messwerte_auswerten.java
+++ src/Messwerte_auswerten.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 public class Messwerte_auswerten {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         float[] anz;
@@ -14,17 +15,32 @@ public class Messwerte_auswerten {
         for (int i = 0; i < anz.length; i++) {
             wert = askWert(in);
 
-            while (wert < -16 || wert > 27){
+            while (wert < -16 || wert > 27) {
                 System.out.println("Geben Sie bitte ein Messwert aus dem Bereich -16.0 bis 27.0 ein");
                 wert = askWert(in);
             }
             anz[i] = wert;
         }
+        float kleinste = anz[0];
+        float groesste = anz[0];
+        for (int i = 1; i < anz.length; i++) {
+            if (anz[i] > groesste)
+                groesste = anz[i];
+            else if (anz[i] < kleinste)
+                kleinste = anz[i]; }
+
         System.out.println("Ihre Werte:");
         int n = 1;
         for (float i : anz) {
             System.out.print("Wert" + " " + (n++) + " " + "=" + " " + i + "; ");
         }
+        System.out.println(" ");
+
+        System.out.println("Der größte Messwert ist: " + groesste);
+        System.out.println("Der kleinste Messwert ist : " + kleinste);
+
+        //System.out.println("Der Position von " + groesste + " ist " + (float)[anz, groesste));
+       // System.out.println("Der Position von " + kleinste + " ist " + (float[anz, kleinste));
     }
 
     private static float askWert(Scanner in) {
@@ -33,10 +49,14 @@ public class Messwerte_auswerten {
     }
 
     private static float[] askMesswert(Scanner in) {
-        System.out.println("Wieviele Messwerte wollen Sie eingebens?");
+        System.out.println("Wieviele Messwerte wollen Sie eingeben?");
         return new float[(int) in.nextFloat()];
     }
-}
+
+
+     }
+
+
 
 
 
