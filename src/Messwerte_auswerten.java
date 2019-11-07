1d9dc345d08c241f62e69f85c83ec2c88b24f378
kate <you@example.com>
2019-11-07 10:23:13 +0100

    added zufall

--- src/Messwerte_auswerten.java
+++ src/Messwerte_auswerten.java
@@ -1,89 +1,120 @@
 import java.util.Scanner;
+import java.lang.Math;
+
 public class Messwerte_auswerten {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        float[] anz;
-        float wert;
 
-        anz = askMesswert(in);
+        String weiterend;
+        do {
 
-        while (anz.length < 5 || anz.length > 40) {
-            System.out.println("Geben Sie bitte eine Zahl aus dem Bereich 5 bis 40 ein");
+            System.out.println("Messwerte auswerten Programm");
+
+            Scanner in = new Scanner(System.in);
+            double[] anz;
+            double wert;
+
             anz = askMesswert(in);
-        }
-        for (int i = 0; i < anz.length; i++) {
-            wert = askWert(in);
 
-            while (wert < -16 || wert > 27) {
-                System.out.println("Geben Sie bitte ein Messwert aus dem Bereich -16.0 bis 27.0 ein");
-                wert = askWert(in);
-            }
-            anz[i] = wert;
-        }
-        float mittelwert = (float) 0.0;
-        float sum = (float) 0.0;
-        float quadAbw = 0;
-        for (int i = 0; i < anz.length; i++) {
-            sum = sum + anz[i];
-            mittelwert = sum / anz.length;
+            while (anz.length < 5 || anz.length > 40) {
+                System.out.println("Geben Sie bitte eine Zahl aus dem Bereich 5 bis 40 ein");
+                anz = askMesswert(in);
             }
 
-        quadAbw = (float) 0.0;
-        for (int i = 0; i < anz.length; i++) {
-            quadAbw = (float) (((anz[i] - mittelwert) * (anz[i] - mittelwert)) / (anz.length - 1.0));
-        }
+            System.out.println("Wollen Sie mit Zufallszahlen arbeiten? (j/n)");
+            String decision = in.next();
+            if (decision.equals("j")) {
+                for (int i = 0; i < anz.length; i++) {
+                    double max = 27.0;
+                    double min = -16.0;
+                    double range = max - min + 1;
+                    double zufall = (Math.random() * range) + min;
+                    anz[i] = zufall;
+                }
 
-        int neg = 0;
-        for(int i = 0; i < anz.length; i++) {
-            if (anz[i] < 0) neg++;
-        }
+            } else
 
-        float kleinste = anz[0];
-        float groesste = anz[0];
-        for (int i = 1; i < anz.length; i++) {
-            if (anz[i] > groesste)
-                groesste = anz[i];
-            else if (anz[i] < kleinste)
-                kleinste = anz[i];
-        }
+                for (int i = 0; i < anz.length; i++) {
+                    wert = askWert(in);
 
-        System.out.println(" ");
-        System.out.println("Ihre Werte:");
-        int n = 1;
-        for (float i : anz) {
-            System.out.print("Wert" + " " + (n++) + " " + "=" + " " + i + "; ");
-        }
-        System.out.println(" ");
+                    while (wert < -16 || wert > 27) {
+                        System.out.println("Geben Sie bitte ein Messwert aus dem Bereich -16.0 bis 27.0 ein");
+                        wert = askWert(in);
+                    }
+                    anz[i] = wert;
+                }
+            double mittelwert = 0.0;
+            double sum = 0.0;
+            for (double v : anz) {
+                sum = sum + v;
+                mittelwert = sum / anz.length;
+            }
 
-        System.out.println(" ");
-        System.out.println("Der größte Messwert ist " + groesste);
-        System.out.println("Der kleinste Messwert ist " + kleinste);
+            double quadAbw = 0.0;
+            double streuung = 0.0;
+            for (double v : anz) {
+                quadAbw = (((v - mittelwert) * (v - mittelwert)) / (anz.length - 1.0) + quadAbw);
+                streuung = Math.sqrt(quadAbw);
+            }
 
-        System.out.println(" ");
-        System.out.println("Der Position von " + groesste + " ist " + position(anz, groesste));
-        System.out.println("Der Position von " + kleinste + " ist " + position(anz, kleinste));
+            int neg = 0;
+            for (double v : anz) {
+                if (v < 0) neg++;
+            }
 
-        System.out.println(" ");
-        System.out.println("Die Mittelwert ist: " + mittelwert);
-        System.out.println("Die  Streuung ist:" + quadAbw);
+            double kleinste = anz[0];
+            double groesste = anz[0];
+            for (int i = 1; i < anz.length; i++) {
+                if (anz[i] > groesste)
+                    groesste = anz[i];
+                else if (anz[i] < kleinste)
+                    kleinste = anz[i];
+            }
 
-        System.out.println(" ");
-        System.out.println("Die Anzahl der negativen Messwete ist " + neg);
+            System.out.println(" ");
+            System.out.println("Ihre Werte:");
+            int n = 1;
+            for (double i : anz) {
+                System.out.print("Wert" + " " + (n++) + " " + "=" + " " + i + "| ");
+                if (n % 9 == 0)
+                    System.out.println();
+            }
+            System.out.println(" ");
+
+            System.out.println(" ");
+            System.out.println("Der größte Messwert ist " + groesste);
+            System.out.println("Der kleinste Messwert ist " + kleinste);
+
+            System.out.println(" ");
+            System.out.println("Der Position von " + groesste + " ist " + position(anz, groesste));
+            System.out.println("Der Position von " + kleinste + " ist " + position(anz, kleinste));
+
+            System.out.println(" ");
+            System.out.println("Die Mittelwert ist: " + mittelwert);
+            System.out.println("Die  Streuung ist:" + streuung);
+
+            System.out.println(" ");
+            System.out.println("Die Anzahl der negativen Messwete ist " + neg);
+
+            System.out.println(" ");
+            System.out.println("Wollen Sie mit anderen Messwerten fortfahren? (j/n)");
+            weiterend = in.next();
+
+        } while (weiterend.equals("j"));
     }
 
-    private static float askWert(Scanner in) {
+    private static double askWert(Scanner in) {
         System.out.println("Geben Sie bitte ein Messwert ein");
         return in.nextFloat();
     }
 
 
-    private static float[] askMesswert(Scanner in) {
+    private static double[] askMesswert(Scanner in) {
         System.out.println("Wieviele Messwerte wollen Sie eingeben?");
-        return new float[(int) in.nextFloat()];
+        return new double[(int) in.nextFloat()];
     }
 
-    private static int position(float[] anz, float t) {
+    private static int position(double[] anz, double t) {
         if (anz == null) return -1;
         int length = anz.length;
         int i = 0;
